4adf2cb83ff712db2b2c16e705743e2f2a048fdf
GabrielLudu <luduzada@gmail.com>
2025-05-19 20:29:29 -0300

    Primeiro Commit

---
+++ scripts.js
@@ -0,0 +1,50 @@
+const uploadBtn = document.querySelector(".upload-btn");
+const inputUpload = document.querySelector(".imagem-upload");
+
+uploadBtn.addEventListener("click", () => {
+  inputUpload.click();
+});
+function lerConteudoDoArquivo(arquivo) {
+  return new Promise((resolve, reject) => {
+    const leitor = new FileReader();
+    leitor.onload = () => {
+      resolve({ url: leitor.result, nome: arquivo.name });
+    };
+    leitor.onerror = () => {
+      reject(`Erro ao ler o arquivo ${arquivo.name}`);
+    };
+    leitor.readAsDataURL(arquivo);
+  });
+}
+
+const imagemPrincipal = document.querySelector(".main-imagem");
+const nomeImagem = document.querySelector(".container-imagem-nome p");
+
+inputUpload.addEventListener("change", async (evento) => {
+  const arquivo = evento.target.files[0];
+  if (arquivo) {
+    try {
+      const conteudoDoArquivo = await lerConteudoDoArquivo(arquivo);
+      imagemPrincipal.src = conteudoDoArquivo.url;
+      nomeImagem.innerHTML = conteudoDoArquivo.nome;
+    } catch (erro) {
+      console.error("Erro na leitura do arquivo");
+    }
+  }
+});
+
+const inputTags = document.querySelector(".input-tags");
+const listaTags = document.getElementById("categoria");
+
+inputTags.addEventListener("keypress", (evento) => {
+  if (evento.key === "Enter") {
+    evento.preventDefault();
+    const tagTexto = inputTags.value.trim();
+    if (tagTexto !== "") {
+      const tagNova = document.createElement("li");
+      tagNova.innerHTML = `<p>${tagTexto}</p> <img src="./img/close-black.svg" class="remove-tag">`;
+      listaTags.appendChild(tagNova);
+      inputTags.value = "";
+    }
+  }
+});
